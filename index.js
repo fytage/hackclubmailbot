640c40bdc07c86e4be1f5fc74d1f7b561a77a125
Šimon Fytage <68707985+fytage@users.noreply.github.com>
2025-09-21 09:55:16 +0000

    fix interaction handling

--- index.js
+++ index.js
@@ -26,6 +26,28 @@ for (const file of commandFiles) {
     }
 }
 
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction, pool);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
+});
+
 client.once('ready', () => {
     console.log('Bot is online!');
     setInterval(checkMailUpdates, 20 * 60 * 1000); // 20 minutes
